c93feb7e2ba218d21c0a293e33f924aea3b4adf4
Daniel B. <boger.daniel@outlook.de>
2026-04-13 21:50:48 +0200

    initialize TreeCapitator plugin with Gradle setup and configuration files

---
+++ src/main/java/user/meistertisch/treeCapitator/TreeCapitator.java
@@ -0,0 +1,16 @@
+package user.meistertisch.treeCapitator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TreeCapitator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
