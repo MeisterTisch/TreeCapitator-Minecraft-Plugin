97a986d7257c2eb0c4fba3ceb248ed2a956952f4
Daniel B. <boger.daniel@outlook.de>
2026-04-16 15:12:55 +0200

    implement simple algorithm for breaking surrounding oak logs

--- src/main/java/user/meistertisch/treeCapitator/event/EventBlockBreak.java
+++ src/main/java/user/meistertisch/treeCapitator/event/EventBlockBreak.java
@@ -3,7 +3,6 @@ package user.meistertisch.treeCapitator.event;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -16,38 +15,44 @@ public class EventBlockBreak implements Listener {
     public void onBlockBreak(BlockBreakEvent event){
         Material blockType = event.getBlock().getType();
 
-        // TODO: Change Check to block tag check instead of materialbtop
+        // TODO: Change Check to block tag check instead of material
         if(blockType == Material.OAK_LOG) {
             destroyBlock(event.getBlock(), event.getPlayer().getInventory().getItemInMainHand());
         }
     }
 
+    int i = 0;
     private void destroyBlock(Block block, ItemStack tool){
+        // Return if block is air
+        if(block.getType().isAir()){
+            return;
+        }
+
+        if(i>100){
+            return;
+        }
+
+        // Safe block type for later check
         Material blockType = block.getType();
+
         block.breakNaturally(tool, true, true); // TODO: Durability not reducing yet
 
-        Block[] neighbors = getBlockRelatives(block);
+        for (int y = -1; y <= 1; y++) {
+            for (int z = -1; z <= 1; z++) {
+                for (int x = -1; x <= 1; x++) {
+                    //Skip the original block
+                    if(x == 0 && y == 0 && z == 0){
+                        continue;
+                    }
 
-        for(Block neighborBlock : neighbors){
-            if(neighborBlock.getType() == blockType){
-                Bukkit.getScheduler().runTaskLater(TreeCapitator.getPlugin(), () -> {
-                    //TODO: Important: Need a cancel if no neighboring block is the same type.
-//                    destroyBlock(neighborBlock, tool);
-                }, 10L);
+                    // Get relatives, check for same type as original block. If same, recursive call of methode with delay for smooth gameplay
+                    Block neighborBlock = block.getRelative(x, y, z);
+                    if (neighborBlock.getType() == blockType) {
+                        Bukkit.getScheduler().scheduleSyncDelayedTask(TreeCapitator.getPlugin(), () -> destroyBlock(neighborBlock, tool),
+                                5+5*(Math.abs(x) +  Math.abs(y) + Math.abs(z))); // The outer the block, the later it breaks; TODO: Make Speed variable for user preferences
+                    }
+                }
             }
         }
-    }
-
-    private Block[] getBlockRelatives(Block block){
-        Block[] neighbors = new Block[6];
-
-        neighbors[0] = block.getRelative(BlockFace.DOWN);
-        neighbors[1] = block.getRelative(BlockFace.UP);
-        neighbors[2] = block.getRelative(BlockFace.NORTH);
-        neighbors[3] = block.getRelative(BlockFace.EAST);
-        neighbors[4] = block.getRelative(BlockFace.SOUTH);
-        neighbors[5] = block.getRelative(BlockFace.WEST);
-
-        return neighbors;
     }
 }
